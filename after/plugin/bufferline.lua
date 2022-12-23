38389e974dc264fee66e7aa35df0e5c60fdbbde3
Muhammad Atpur Rafif <muhammadatpurrafif@gmail.com>
2022-12-23 14:04:55 +0700

    Ignore terminal in bufferline

--- after/plugin/bufferline.lua
+++ after/plugin/bufferline.lua
@@ -6,12 +6,32 @@ end
 vim.keymap.set("n", "<M-H>", ":BufferLineCyclePrev<CR>", {})
 vim.keymap.set("n", "<M-L>", ":BufferLineCycleNext<CR>", {})
 
+function dump(o)
+   if type(o) == 'table' then
+      local s = '{ '
+      for k,v in pairs(o) do
+         if type(k) ~= 'number' then k = '"'..k..'"' end
+         s = s .. '['..k..'] = ' .. dump(v) .. ','
+      end
+      return s .. '} '
+   else
+      return tostring(o)
+   end
+end
+
 bufferline.setup{
 	options = {
 		show_buffer_close_icons = false,
 		separator_style = "slant",
 		offsets = {
 			{ filetype = "neo-tree", text = "Explorer", padding = 1 }
-		}
+		},
+		custom_filter = function(buf_number, _)
+			if vim.bo[buf_number].buftype == 'terminal' then
+				return false
+			end
+
+			return true
+		end
 	}
 }
