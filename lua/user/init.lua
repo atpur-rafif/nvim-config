344917502946c3b54785ed7b15047ce360eaf9cd
Muhammad Atpur Rafif <muhammadatpurrafif@gmail.com>
2023-01-23 09:09:51 +0700

    Fixed yanking using win32yank instead xclip

--- lua/user/init.lua
+++ lua/user/init.lua
@@ -2,6 +2,20 @@ require("user.packer")
 
 vim.cmd([[
 
+" Yanking
+let g:clipboard = {
+      \   'name': 'win32yank-wsl',
+      \   'copy': {
+      \      '+': 'win32yank.exe -i --crlf',
+      \      '*': 'win32yank.exe -i --crlf',
+      \    },
+      \   'paste': {
+      \      '+': 'win32yank.exe -o --lf',
+      \      '*': 'win32yank.exe -o --lf',
+      \   },
+      \   'cache_enabled': 0,
+      \ }
+
 " Just to make sure folder won't b cluttered by backup and undo file
 set backupdir=~/.vim/backup
 set directory=~/.vim/swap
